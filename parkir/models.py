3ab51423a025c916d4b07a43675cd641a4d98cee
aliefian <shinichi.aliefian@gmail.com>
2021-04-05 15:26:18 +0700

    Tugas Akhir 7

--- parkir/models.py
+++ parkir/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Parkir(models.Model):
     Title = models.CharField(max_length=50)
-    Status = models.BooleanField(default=False)
+    Status = models.IntegerField(default=0)
     
     def __str__(self):
         return self.Title
 
 class Jumlah(models.Model):
     id = models.AutoField(primary_key=True, unique = True)
-    Tersedia = models.IntegerField()
+    Tersedia = models.IntegerField(default=0)
     
     def __str__(self):
         return self.id
