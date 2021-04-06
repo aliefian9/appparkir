5dc671dcc17a6c77480c69e95140fd6bad56de4c
aliefian9 <58259558+aliefian9@users.noreply.github.com>
2021-04-06 13:09:23 +0700

    Update models.py

--- parkir/models.py
+++ parkir/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Parkir(models.Model):
     Title = models.CharField(max_length=50)
-    Status = models.IntegerField(default=0)
+    Status = models.IntegerField(default=1)
     
     def __str__(self):
         return self.Title
 
 class Jumlah(models.Model):
     id = models.AutoField(primary_key=True, unique = True)
-    Tersedia = models.IntegerField(default=1)
+    Tersedia = models.IntegerField(default=0)
     
     def __str__(self):
         return self.id
